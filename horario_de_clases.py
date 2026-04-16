4003dbe024b67fcd4d89272e0e665b1da4c779f2
Bairon Núñez <lensko_2002@hotmail.com>
2026-04-16 01:14:53 -0400

    Update horario_de_clases.py

--- horario_de_clases.py
+++ horario_de_clases.py
@@ -7,6 +7,9 @@ from ortools.sat.python import cp_model as cp
 # Física      - Programación
 # Química     - Programación
 
+# En este caso no usaremos SolutionPrinter ya que no requerimos imprimir múltiples soluciones
+
+
 # Crear modelo
 modelo = cp.CpModel()
 
@@ -17,3 +20,24 @@ quimica = modelo.NewIntVar(1, 3, 'Química')
 programacion = modelo.NewIntVar(1, 3, 'Programación')
 
 
+asignaturas = [matematicas, fisica, quimica, programacion]
+
+# Pares que no pueden ir en el mismo bloque
+comparten_sala = [(matematicas, fisica), (matematicas, quimica), (fisica, programacion), (quimica, programacion)]
+
+# Definimos la restricción
+for r1, r2 in comparten_sala:
+    modelo.Add(r1 != r2)
+
+
+# Definir el solver
+solver = cp.CpSolver()
+status = solver.Solve(modelo)
+
+# Imprimir el mejor resultado posible
+if status == cp.OPTIMAL:
+    for v in asignaturas:
+        if solver.Value(v) == 1: print(v.Name(), '--> Mañana')
+        elif solver.Value(v) == 2: print(v.Name(), '--> Tarde')
+        elif solver.Value(v) == 3: print(v.Name(), '--> Noche')
+    
